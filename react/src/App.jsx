17e5c46eca742f783dd25a79a2279cb3754e2f5d
imaad666 <imaadbinirshad@gmail.com>
2025-11-20 16:54:04 +0530

    rebut

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,38 +1,8 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import HomePage from './pages/HomePage'
-import ArtisanStory from './pages/ArtisanStory'
-import ReturnsPage from './pages/ReturnsPage'
-import PrivacyPolicy from './pages/PrivacyPolicy'
-import ShippingDelivery from './pages/ShippingDelivery'
-import TermsConditions from './pages/TermsConditions'
-import CatalogPage from './pages/CatalogPage'
-import CollectionsPage from './pages/CollectionsPage'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-import './styles/home.css'
-import './styles/catalog.css'
-import './styles/returns.css'
-import './styles/privacy.css'
-import './styles/shipping.css'
-import './styles/terms.css'
-import './styles/collections.css'
+import ComingSoon from './components/ComingSoon'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/catalog" element={<CatalogPage />} />
-        <Route path="/our-story" element={<ArtisanStory />} />
-        <Route path="/collections" element={<CollectionsPage />} />
-        <Route path="/returns" element={<ReturnsPage />} />
-        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-        <Route path="/shipping-delivery" element={<ShippingDelivery />} />
-        <Route path="/terms-conditions" element={<TermsConditions />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
+    <ComingSoon />
   )
 }
 
---
+++ react/src/components/ComingSoon.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/coming-soon.css';
+
+const ComingSoon = () => {
+  return (
+    <div className="coming-soon-container">
+      <img src="/assets/logos/miras_text_logo.png" alt="MIRAS" className="coming-soon-logo" />
+      <div className="coming-soon-text">Coming Soon</div>
+    </div>
+  );
+};
+
+export default ComingSoon;
